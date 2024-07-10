95739562dde50ba86343d5277d16486de29b9935
Tegar Abdullah <t-tegar.abdullah@traveloka.com>
2024-07-10 16:05:00 +0700

    add condition on OFF day

--- insert_logbook.py
+++ insert_logbook.py
@@ -17,11 +17,17 @@ def insert(logbook):
     date = logbook.get("date")
     title = logbook.get("title")
     desc = logbook.get("desc")
+    clock_in = "09:00 am"
+    clock_out = "06:00 pm"
+
+    if title == "OFF" and desc == "OFF":
+        clock_in = "OFF"
+        clock_out = "OFF"
 
     month = int(date.split("-")[1])
     logbook_header_id = months[month - month_start_from]
 
-    payload = f"model%5BID%5D=00000000-0000-0000-0000-000000000000&model%5BLogBookHeaderID%5D={logbook_header_id}&model%5BDate%5D={date}T00%3A00%3A00&model%5BActivity%5D={title}&model%5BClockIn%5D=09%3A00+am&model%5BClockOut%5D=06%3A00+pm&model%5BDescription%5D={desc}&model%5Bflagjulyactive%5D=false"
+    payload = f"model%5BID%5D=00000000-0000-0000-0000-000000000000&model%5BLogBookHeaderID%5D={logbook_header_id}&model%5BDate%5D={date}T00%3A00%3A00&model%5BActivity%5D={title}&model%5BClockIn%5D={clock_in}&model%5BClockOut%5D={clock_out}&model%5BDescription%5D={desc}&model%5Bflagjulyactive%5D=false"
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
